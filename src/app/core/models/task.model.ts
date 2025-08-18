25dcea2ffa0be84909f40bd616b77ef7b534f4af
Arctic Fox <tyskanhik@mail.ru>
2025-08-18 23:29:03 +0300

    create task model

---
+++ src/app/core/models/task.model.ts
@@ -0,0 +1,24 @@
+export const TaskCategory = {
+  STUDIES: 'Учеба',
+  WORK: 'Работа',
+  HOME: 'Дом'
+} as const;
+export type TaskCategory = typeof TaskCategory[keyof typeof TaskCategory];
+
+export const TaskPriority = {
+  LOW: 'low',
+  MEDIUM: 'medium',
+  HIGHT: 'hight'
+} as const;
+export type TaskPriority = typeof TaskPriority[keyof typeof TaskPriority];
+
+export interface Task {
+  id: string;
+  title: string;
+  category: TaskCategory;
+  priority: TaskPriority;
+  completed: boolean;
+  createdAt: Date;
+}
+
+export type NewTask = Omit<Task, 'id' | 'createdAt' | 'completed'>;
